f069a85068ef3da3ceb9b18df88c825a5ba5823c
Lisa <misslisazhou@gmail.com>
2016-06-28 17:50:16 -1000

    create function to translate pig latin to english

--- pig-latin.js
+++ pig-latin.js
@@ -1,28 +1,48 @@
-function pigToEnglish(engString){
+function englishToPig(engString){
   if (typeof engString !== 'string'){
     throw new Error();
   }
-
-  //splits string into an array
-  var splitUp = engString.split(/([aeiou].*)/);
-
-  //this string starts with a vowel
-  if (splitUp[0].length === 0){
-    splitUp[1] += 'ay';
-  }else{ //this string begins with a consonant
-    splitUp[0] += 'ay';
-    splitUp[1] += '-';
-    splitUp.reverse();
+  //1. seperate sentence into array
+  var seperate = engString.split(' ');
+  for (var i = 0; i < seperate.length; i++){
+    //2. take each word and split based on if it starts with vowel or consonant
+    var splitUp = seperate[i].split(/([aeiouAEIOU].*)/);
+    //this string starts with a vowel
+    if (splitUp[0].length === 0){
+      splitUp[1] += 'ay';
+    }else{ //this string begins with a consonant
+      splitUp[0] += 'ay';
+      splitUp[1] += '-';
+      splitUp.reverse();
+    }
+    //3. put the word back together
+    seperate[i] = splitUp.join('');
   }
-
-  return splitUp.join('');
+  //4. put the sentence back together
+  return seperate.join(' ');
 }
 
-function englishToPig(pigString) {
-  // this converts english string to pig latin
+function pigToEnglish(pigString) {
+  if (typeof pigString !== 'string'){
+    throw new Error();
+  }
+  //1. seperate sentence into array
+  var seperate = pigString.split(' ');
+  //2. take each word and remove/move the end based on if appended with "ay" or "'-' + consonant + 'ay'"
+  for (var i = 0; i < seperate.length; i++) {
+    //this will be an English word that begins with a vowel
+    if (seperate[i].indexOf('-') === -1){
+      seperate[i] = seperate[i].slice(0, -2);
+    }else{ //this will be an English word that begins with a consonant
+      var clippedEnd = seperate[i].slice(0, -2);
+      seperate[i] = clippedEnd.split('-').reverse().join('');
+    }
+  }
+  //3. put the sentence back together
+  return seperate.join(' ');
 }
 
 module.exports = {
-  pigToEnglish: pigToEnglish,
-  englishToPig: englishToPig
+  englishToPig: englishToPig,
+  pigToEnglish: pigToEnglish
 };
--- pig-latin.spec.js
+++ pig-latin.spec.js
@@ -3,7 +3,39 @@ var expect =  chai.expect;
 var pigLatin = require('./pig-latin.js');
 
 describe('Pig Latin Translator', function(){
+  describe('English to Pig Latin Function', function () {
+    it('should be a function', function () {
+      expect(pigLatin.englishToPig).to.be.a('function');
+    });
+    it('should only accept a string as an argument', function () {
+      expect(pigLatin.englishToPig.bind(null)).to.throw();
+    });
+    it('should append "ay" to the end of words that start with a vowel', function () {
+      var eat = pigLatin.englishToPig('eat');
+      expect(eat).to.equal('eatay');
+      var omelet = pigLatin.englishToPig('omelet');
+      expect(omelet).to.equal('omeletay');
+      var are = pigLatin.englishToPig('are');
+      expect(are).to.equal('areay');
+    });
+    it('should move consonant letters up to the first vowel to the end of the word and append "ay" to words that start with a consonant', function () {
+      var pig = pigLatin.englishToPig('pig');
+      expect(pig).to.equal('ig-pay');
+      var banana = pigLatin.englishToPig('banana');
+      expect(banana).to.equal('anana-bay');
+      var trash = pigLatin.englishToPig('trash');
+      expect(trash).to.equal('ash-tray');
+    });
+    it('should convert all words in a sentence to Pig Latin following the rules of whether they start with a vowel or consonant', function () {
+      var sentence = pigLatin.englishToPig('I am here');
+      expect(sentence).to.equal('Iay amay ere-hay');
+      var question = pigLatin.englishToPig('Are we there yet');
+      expect(question).to.equal('Areay e-way ere-thay et-yay');
+    });
+  });
+});
 
+describe('English Translator', function(){
   describe('Pig Latin to English Function', function () {
     it('should be a function', function () {
       expect(pigLatin.pigToEnglish).to.be.a('function');
@@ -11,21 +43,27 @@ describe('Pig Latin Translator', function(){
     it('should only accept a string as an argument', function () {
       expect(pigLatin.pigToEnglish.bind(null)).to.throw();
     });
-    it('should append "ay" to the end of words that start with a vowel', function () {
-      var eat = pigLatin.pigToEnglish('eat');
-      expect(eat).to.equal('eatay');
-      var omelet = pigLatin.pigToEnglish('omelet');
-      expect(omelet).to.equal('omeletay');
-      var are = pigLatin.pigToEnglish('are');
-      expect(are).to.equal('areay');
+    it('should remove "ay" to the end of words that have "ay" appended', function () {
+      var eatPTE = pigLatin.pigToEnglish('eatay');
+      expect(eatPTE).to.equal('eat');
+      var omeletPTE = pigLatin.pigToEnglish('omeletay');
+      expect(omeletPTE).to.equal('omelet');
+      var arePTE = pigLatin.pigToEnglish('areay');
+      expect(arePTE).to.equal('are');
     });
-    it('should move consonant letters up to the first vowel to the end of the word and append "ay" to words that start with a consonant', function () {
-      var pig = pigLatin.pigToEnglish('pig');
-      expect(pig).to.equal('ig-pay');
-      var banana = pigLatin.pigToEnglish('banana');
-      expect(banana).to.equal('anana-bay');
-      var trash = pigLatin.pigToEnglish('trash');
-      expect(trash).to.equal('ash-tray');
+    it('should move consonant(s) to the from of the word after "-" as well as remove the "-"', function () {
+      var pigPTE = pigLatin.pigToEnglish('ig-pay');
+      expect(pigPTE).to.equal('pig');
+      var bananaPTE = pigLatin.pigToEnglish('anana-bay');
+      expect(bananaPTE).to.equal('banana');
+      var trashPTE = pigLatin.pigToEnglish('ash-tray');
+      expect(trashPTE).to.equal('trash');
+    });
+    it('should convert all words in a sentence to Pig Latin following the rules of whether they start with a vowel or consonant', function () {
+      var sentencePTE = pigLatin.pigToEnglish('Iay amay ere-hay');
+      expect(sentencePTE).to.equal('I am here');
+      var questionPTE = pigLatin.pigToEnglish('Areay e-way ere-thay et-yay');
+      expect(questionPTE).to.equal('Are we there yet');
     });
   });
 });
