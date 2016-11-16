2002812fb12917642d10c4bcb201bef654705102
Lisa <misslisazhou@gmail.com>
2016-11-16 00:22:13 -1000

    add function to convert english string to pig latin

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-function translateToPig() {
-  alert('test');
+function englishToPig(engString){
+  if (typeof engString !== 'string'){
+    throw new Error();
+  }
+  //1. seperate sentence into array
+  var seperate = engString.split(' ');
+  for (var i = 0; i < seperate.length; i++){
+    //2. take each word and split based on if it starts with vowel or consonant
+    var splitUp = seperate[i].split(/([aeiouAEIOU].*)/);
+    //this string starts with a vowel
+    if (splitUp[0].length === 0){
+      splitUp[1] += 'ay';
+    }else{ //this string begins with a consonant
+      splitUp[0] += 'ay';
+      splitUp[1] += '-';
+      splitUp.reverse();
+    }
+    //3. put the word back together
+    seperate[i] = splitUp.join('');
+  }
+  //4. put the sentence back together
+  return seperate.join(' ');
 }
